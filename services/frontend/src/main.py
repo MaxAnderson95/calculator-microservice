4b78c3bb5686c01780c0ce663a5bef6d328d9f5c
Max Anderson <max.a.anderson95@gmail.com>
2023-12-12 21:30:38 -0500

    Added error handling

--- services/frontend/src/main.py
+++ services/frontend/src/main.py
@@ -5,7 +5,7 @@ import uvicorn
 from typing import Annotated
 from fastapi import FastAPI, HTTPException, Form
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
+import controller
 
 LEVEL = logging.DEBUG if os.getenv("DEBUG") else logging.INFO
 logging.basicConfig(level=LEVEL)
@@ -15,34 +15,30 @@ app = FastAPI(title="Frontend Service")
 
 
 @app.post("/api/v1/add")
-def add(num1: Annotated[float, Form()], num2: Annotated[float, Form()]):
+def add(num1: Annotated[float, Form()], num2: Annotated[float, Form()]) -> float:
     logger.info(f"Adding {num1} + {num2}")
-    result = num1 + num2
-    return result
+    return controller.add(num1, num2)
 
 
 @app.post("/api/v1/subtract")
-def subtract(num1: Annotated[float, Form()], num2: Annotated[float, Form()]):
+def subtract(num1: Annotated[float, Form()], num2: Annotated[float, Form()]) -> float:
     logger.info(f"Subtracting {num1} - {num2}")
-    result = num1 - num2
-    return result
+    return controller.subtract(num1, num2)
 
 
 @app.post("/api/v1/multiply")
-def multiply(num1: Annotated[float, Form()], num2: Annotated[float, Form()]):
+def multiply(num1: Annotated[float, Form()], num2: Annotated[float, Form()]) -> float:
     logger.info(f"Multiplying {num1} * {num2}")
-    result = num1 * num2
-    return result
+    return controller.multiply(num1, num2)
 
 
 @app.post("/api/v1/divide")
-def divide(num1: Annotated[float, Form()], num2: Annotated[float, Form()]):
+def divide(num1: Annotated[float, Form()], num2: Annotated[float, Form()]) -> float:
     logger.info(f"Dividing {num1} / {num2}")
-    if num2 == 0:
-        logger.error("Cannot divide by zero")
-        raise HTTPException(status_code=400, detail="Cannot divide by zero")
-    result = num1 / num2
-    return result
+    try:
+        return controller.divide(num1, num2)
+    except ZeroDivisionError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 static_folder = pathlib.Path(__file__).parent.resolve() / "static"
