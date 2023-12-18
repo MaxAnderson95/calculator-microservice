a10a5dba4481b951bf5f953b6957c62eeca903b8
Max Anderson <max.a.anderson95@gmail.com>
2023-12-17 21:12:16 -0500

    Added automatic and manual instrumentation

--- services/frontend/src/main.py
+++ services/frontend/src/main.py
@@ -1,34 +1,40 @@
 import logging
 import pathlib
 import uvicorn
+import controller
 from typing import Annotated
 from fastapi import FastAPI, HTTPException, Form
 from fastapi.staticfiles import StaticFiles
 from config import settings, ALLOWED_OPERATIONS
-import controller
 from db_logging import new_calculation_log
+from opentelemetry import trace
 
 logging.basicConfig(level=settings.LEVEL)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title="Frontend Service")
+tracer = trace.get_tracer(__name__)
 
 
 @app.post("/api/v1/calculate")
 def calculate(operation: Annotated[str, Form()], num1: Annotated[float, Form()], num2: Annotated[float, Form()]) -> float:
-    logger.info(f"Received operation: {operation} with values: {num1} and {num2}")
-    if operation not in ALLOWED_OPERATIONS:
-        raise HTTPException(status_code=404, detail="Invalid operation")
+    with tracer.start_as_current_span("calculate") as span:
+        logger.info(f"Received operation: {operation} with values: {num1} and {num2}")
+        span.set_attribute("operation", operation)
+        span.set_attribute("num1", num1)
+        span.set_attribute("num2", num2)
+        if operation not in ALLOWED_OPERATIONS:
+            raise HTTPException(status_code=404, detail="Invalid operation")
 
-    try:
-        result, cache_hit = controller.calculate(operation, num1, num2)
-        new_calculation_log(operation, num1, num2, result, cache_hit)
-        return result
-    except controller.CalculatorError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    except Exception as e:
-        logger.error(e)
-        raise HTTPException(status_code=500, detail="An unknown error occurred")
+        try:
+            result, cache_hit = controller.calculate(operation, num1, num2)
+            new_calculation_log(operation, num1, num2, result, cache_hit)
+            return result
+        except controller.CalculatorError as e:
+            raise HTTPException(status_code=400, detail=str(e))
+        except Exception as e:
+            logger.error(e)
+            raise HTTPException(status_code=500, detail="An unknown error occurred")
 
 
 static_folder = pathlib.Path(__file__).parent.resolve() / "static"
